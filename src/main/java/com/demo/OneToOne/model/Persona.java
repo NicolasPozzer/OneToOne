978aa26fe28e8085e325520ee11ddd19d3e64cf3
NicolasPozzer <pozzernico@gmail.com>
2023-10-04 12:36:00 -0300

    publish git

---
+++ src/main/java/com/demo/OneToOne/model/Persona.java
@@ -0,0 +1,32 @@
+package com.demo.OneToOne.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    @OneToOne
+    @JoinColumn (name = "una_mascota_id_mascota",
+            referencedColumnName = "id_mascota")
+    private Mascota unaMascota;
+
+    public Persona() {
+    }
+
+    public Persona(Long id, String nombre, String apellido, int edad, Mascota unaMascota) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.unaMascota = unaMascota;
+    }
+}
